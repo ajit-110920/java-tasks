4ea765de237e532c0788a7f2505eb0afdfe84890
lENOVO <ajitbhandekar110920@gmail.com>
2026-02-15 16:26:18 +0530

    day3task3

---
+++ day3task3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class day3task3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter units consumed: ");
+        int units = input.nextInt();
+        double totalBill = 0;
+        if (units <= 100) {
+            totalBill = units * 5; 
+        } 
+        else if (units <= 200) {
+            totalBill = (100 * 5) + (units - 100) * 7;
+        } 
+        else {
+            totalBill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+        }
+        System.out.println("Total Bill Amount: ₹" + totalBill);
+    
+        input.close();
+    }
+}
